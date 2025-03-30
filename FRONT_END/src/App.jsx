eeb01e77ab1da3f699d1e4f6ad0d4f02a244855d
runjan2401 <runjanramteke2401@gmail.com>
2025-03-30 12:25:53 +0530

    Fixed import issue and updated Netlify config

--- FRONT_END/src/App.jsx
+++ FRONT_END/src/App.jsx
@@ -1,17 +1,17 @@
 import React, { useContext, useEffect } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home";
-import Appointment from "./Pages/Appointment";
-import AboutUs from "./Pages/AboutUs";
-import Register from "./Pages/Register";
-import Footer from "./components/Footer";
-import Navbar from "./components/Navbar";
+import Home from "./Pages/Home.jsx";
+import Appointment from "./Pages/Appointment.jsx";
+import AboutUs from "./Pages/AboutUs.jsx";
+import Register from "./Pages/Register.jsx";
+import Footer from "./components/Footer.jsx";
+import Navbar from "./components/Navbar.jsx";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import axios from "axios";
 import { Context } from "./main";
-import Login from "./Pages/Login";
+import Login from "./Pages/Login.jsx";
 const App = () => {
   const { isAuthenticated, setIsAuthenticated, setUser } =
     useContext(Context);
